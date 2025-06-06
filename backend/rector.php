f8bc98b7b01ee4281a85abf9883e3e360ab2f8e1
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-06-06 16:45:41 +0600

    Install rector

---
+++ backend/rector.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Set\LaravelLevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $config): void {
+    $config->paths([
+        __DIR__ . '/app',
+        __DIR__ . '/routes',
+        __DIR__ . '/database',
+        __DIR__ . '/tests',
+    ]);
+
+    $config->sets([
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+
+        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_IF_HELPERS,
+
+        SetList::DEAD_CODE,
+        SetList::PHP_82,
+    ]);
+};
