771d7e40cb0a8bf9a292a68c5f6ec1b5b5a7faa6
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2026-01-12 20:41:46 +0600

    Install and config NativeWind with expo

---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-red-300 text-bold">
+      <Text className="font-bold">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
